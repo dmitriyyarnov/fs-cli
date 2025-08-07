48d7de06542b9dd1c9bc422f32d421ac5bd634fe
dmitriyyarnov <78679511+dmitriyyarnov@users.noreply.github.com>
2025-08-07 03:16:03 +0300

    rename

--- feature/count.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-def count_files(path):
-    path_obj = Path(path)
-    if not path_obj.is_dir():
-        print(f"'{path}' не является папкой.")
-        return 0
-
-    return sum(1 for _ in path_obj.rglob('*') if _.is_file())
--- main.py
+++
@@ -1,4 +0,0 @@
-from manager.cli import main
-
-if __name__ == "__main__":
-    main()
